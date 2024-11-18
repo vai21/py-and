4cdbf329c12b1172e1610c00a91188451d74962d
Faisal <moch.rasid@anugrah-argon.com>
2024-11-18 10:42:06 +0700

    remove customer from serializer

--- bphis/bp/serializers.py
+++ bphis/bp/serializers.py
@@ -5,4 +5,4 @@ from rest_framework import serializers
 class BpSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Bp
-        fields = ['id', 'systolic', 'diastolic', 'meanarterialpressure', 'pulserate', 'ihb', 'is_user_move', 'retest', 'measurement_time', 'created_at', 'customer', 'group_id']
+        fields = ['id', 'systolic', 'diastolic', 'meanarterialpressure', 'pulserate', 'ihb', 'is_user_move', 'retest', 'measurement_time', 'created_at', 'group_id']
