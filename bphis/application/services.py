e1e3d2ca8df52c3b3fe9c77087a8821207984a4e
Faisal <moch.rasid@anugrah-argon.com>
2024-12-09 09:55:42 +0700

    update push to open api codes

--- bphis/application/services.py
+++ bphis/application/services.py
@@ -4,17 +4,16 @@ import requests
 def hitOpenApi(data):
   # url open api
   url = 'http://localhost:8000/api/bp/'
-
+  print(f"payload data: {data}")
   result = requests.post(url, data={
-    'systolic': data.systolic,
-    'diastolic': data.diastolic,
-    'pulserate': data.pulse_rate,
-    'ihb': data.ihb or None,
-    'meanarterialpressure': data.map or None,
-    'is_user_move': data.is_user_move or None,
-    'retest': data.retest or None,
-    'measurement_time': data.measurement_time or None,
-    'created_at': data.date or None
+    'systolic': data.get('systolic'),
+    'diastolic': data.get('diastolic'),
+    'pulserate': data.get('pulserate'),
+    'ihb': data.get('ihb') or None,
+    'meanarterialpressure': data.get('map') or None,
+    'is_user_move': data.get('is_user_move') or None,
+    'retest': data.get('retest') or None,
+    'measurement_time': data.get('measurement_time') or None,
+    'created_at': data.get('date') or None
   })
-  
-  print(result.text)
+  print(f"result {result.text}")
