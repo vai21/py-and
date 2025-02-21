152b56898f33a31a73d194784729c49e3d6e06d0
Faisal <moch.rasid@anugrah-argon.com>
2025-02-21 11:05:40 +0700

    chore: update requirements, startup, firstrun

--- bphis/application/services.py
+++ bphis/application/services.py
@@ -5,15 +5,15 @@ def hitOpenApi(data):
   # url open api
   url = 'http://localhost:8000/api/bp/'
   print(f"payload data: {data}")
-  result = requests.post(url, data={
-    'systolic': data.get('systolic'),
-    'diastolic': data.get('diastolic'),
-    'pulserate': data.get('pulserate'),
-    'ihb': data.get('ihb') or None,
-    'meanarterialpressure': data.get('map') or None,
-    'is_user_move': data.get('is_user_move') or None,
-    'retest': data.get('retest') or None,
-    'measurement_time': data.get('measurement_time') or None,
-    'created_at': data.get('date') or None
-  })
-  print(f"result {result.text}")
+  # result = requests.post(url, data={
+  #   'systolic': data.get('systolic'),
+  #   'diastolic': data.get('diastolic'),
+  #   'pulserate': data.get('pulserate'),
+  #   'ihb': data.get('ihb') or None,
+  #   'meanarterialpressure': data.get('map') or None,
+  #   'is_user_move': data.get('is_user_move') or None,
+  #   'retest': data.get('retest') or None,
+  #   'measurement_time': data.get('measurement_time') or None,
+  #   'created_at': data.get('date') or None
+  # })
+  # print(f"result {result.text}")
---
+++ startup.ps1
@@ -0,0 +1,24 @@
+# Start manage.py runserver as another background process\
+$WorkingDirectory = "D:\moch.rasid_aam\Documents\py-and"
+Set-Location $WorkingDirectory
+.\env\Scripts\activate.ps1
+Set-Location .\bphis
+$server = Start-Process python -ArgumentList "manage.py runserver" -NoNewWindow -PassThru
+
+# Start flask as a background process
+Set-Location .\application
+$runflask = Start-Process python -ArgumentList "app.py" -NoNewWindow -PassThru
+
+# Start celery
+$env:FLOWER_UNAUTHENTICATED_API="true"
+$runceleryworker = Start-Process celery -ArgumentList "-A tasks worker -l info --pool=solo" -NoNewWindow -PassThru
+$runcelerybeat = Start-Process celery -ArgumentList "-A tasks beat -l INFO" -NoNewWindow -PassThru
+$runceleryflower = Start-Process celery -ArgumentList "-A tasks flower" -NoNewWindow -PassThru
+
+
+# Optional: Wait until both processes exit (if you want to ensure they complete)
+Wait-Process -Id $server.Id, $runflask.Id
+
+# Optionally print process IDs
+Write-Host "flask running with PID: $($runflask.Id)"
+Write-Host "django runserver running with PID: $($server.Id)"
