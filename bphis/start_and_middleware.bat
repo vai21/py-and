2eaa2feea449bb590c40134ebeacc2e5d84b4b0c
Faisal <moch.rasid@anugrah-argon.com>
2025-10-07 15:50:08 +0700

    feat: update config bat

--- bphis/start_and_middleware.bat
+++ bphis/start_and_middleware.bat
@@ -5,9 +5,9 @@ call env\Scripts\activate
 cd /d C:\Users\Admin\Workspace\py-and\bphis\application
 set FLOWER_UNAUTHENTICATED_API="true"
 start "Celery Worker" cmd /k celery -A tasks worker -l info --pool=solo
-timeout /t 5 /nobreak
+timeout /t 10 /nobreak
 start "Celery Beat" cmd /k celery -A tasks beat -l INFO
-timeout /t 5 /nobreak
-start "Celery Flower" cmd /k celery -A tasks flower
-timeout /t 5 /nobreak
+timeout /t 10 /nobreak
+@REM start "Celery Flower" cmd /k celery -A tasks flower
+@REM timeout /t 10 /nobreak
 python app.py
