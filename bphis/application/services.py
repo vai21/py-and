1bb33753a638ec0d142931d53c4c8addf3a563eb
Faisal <moch.rasid@anugrah-argon.com>
2025-10-07 13:41:04 +0700

    feat: update api and readme

--- bphis/application/services.py
+++ bphis/application/services.py
@@ -3,7 +3,7 @@ import requests
 
 def hitOpenApi(data):
   # url open api
-  url = 'http://localhost:8000/api/bp/'
+  url = 'http://localhost:8000/api/bp/' # Alamat API Rumah Sakit (HIS)
   print(f"payload data: {data}")
   result = requests.post(url, data={
     'systolic': data.get('systolic'),
@@ -15,7 +15,6 @@ def hitOpenApi(data):
     'retest': data.get('retest') or None,
     'measurement_time': data.get('measurement_time') or None,
     'created_at': data.get('date') or None,
-    'device_id': 'ANDTM01'
+    'device_id': 'AND-01' # Device ID
   })
   print(f"result {result.text}")
-  
