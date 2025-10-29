6d57475acfeace11a47e332efdef704da0186769
Faisal <moch.rasid@anugrah-argon.com>
2025-10-29 11:57:31 +0700

    feat: update startup

--- startup.cmd
+++ startup.cmd
@@ -1,3 +1,3 @@
 PowerShell -Command "Set-ExecutionPolicy Unrestricted" >> "%TEMP%\StartupLog.txt" 2>&1
-PowerShell %USERPROFILE%\Workspace\py-and\env\Scripts\Activate.ps1 >> "%TEMP%\StartupLog.txt" 2>&1
-PowerShell %USERPROFILE%\Workspace\py-and\bphis\runapp.ps1 >> "%TEMP%\StartupLog.txt" 2>&1
+PowerShell %USERPROFILE%\AND\py-and\env\Scripts\Activate.ps1 >> "%TEMP%\StartupLog.txt" 2>&1
+PowerShell %USERPROFILE%\AND\py-and\bphis\runapp.ps1 >> "%TEMP%\StartupLog.txt" 2>&1
